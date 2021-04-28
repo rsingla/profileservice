27134dc2de95b0518f38c51db91e86c2f4d0a9e6
Rajeev Singla <rajeev1.singla@gmail.com>
2021-04-27 17:34:30 -0700

    Added Application properties & Test class

---
+++ src/test/java/io/apicode/profile/ProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.apicode.profile;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProfileServiceApplicationTests {
+
+	public ProfileServiceApplicationTests() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
